e7b9ad0d179a1f33e8212609ced98b47bc63cb87
Michael Chapman <submariner100@gmail.com>
2017-07-13 20:33:04 +0100

    first

---
+++ Project21/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Project21
+//
+//  Created by Macbook on 26/04/2017.
+//  Copyright © 2017 Chappy-App. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          
+          navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+          
+          navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+     
+       }
+     
+     func registerLocal() {
+     
+          func registerLocal() {
+               let center = UNUserNotificationCenter.current()
+               
+               center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+                    
+                    if granted {
+                         print("Yay!")
+                    } else {
+                         print("D'oh")
+                    }
+               }
+          }
+     }
+     
+     func scheduleLocal() {
+     
+          func scheduleLocal() {
+               let center = UNUserNotificationCenter.current()
+               let content = UNMutableNotificationContent()
+               
+               content.title = "Late wake up call"
+               content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+               content.categoryIdentifier = "alarm"
+               content.userInfo = ["customData": "fizzbuzz"]
+               content.sound = UNNotificationSound.default()
+               
+               
+               var dateComponents = DateComponents()
+               dateComponents.hour = 20
+               dateComponents.minute = 20
+               
+               //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+               let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+               
+               let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+               center.add(request)
+          }
+   
+
+     }
+     
+     func registerCategories() {
+          let center = UNUserNotificationCenter.current()
+          center.delegate = self
+          
+          let show = UNNotificationAction(identifier: "show", title: "Tell me more…", options: .foreground)
+          let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+          
+          center.setNotificationCategories([category])
+     }
+     
+     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+          // pull out the buried userInfo dictionary
+          let userInfo = response.notification.request.content.userInfo
+          
+          if let customData = userInfo["customData"] as? String {
+               print("Custom data received: \(customData)")
+               
+               switch response.actionIdentifier {
+               case UNNotificationDefaultActionIdentifier:
+                    // the user swiped to unlock
+                    print("Default identifier")
+                    
+               case "show":
+                    // the user tapped our "show more info…" button
+                    print("Show more information…")
+                    break
+                    
+               default:
+                    break
+               }
+          }
+          
+          // you must call the completion handler when you're done
+          completionHandler()
+     }
+     
+}
+
